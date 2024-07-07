677579aad12f30e3991cd36422d8e312bfca32a8
ardijorganxhi <ardijorganxhiu@gmail.com>
2024-07-08 01:03:58 +0200

    fix jpa auditing

---
+++ src/main/java/dev/ardijorganxhi/shkruajshqip/config/AuditorAwareImpl.java
@@ -0,0 +1,22 @@
+package dev.ardijorganxhi.shkruajshqip.config;
+
+import dev.ardijorganxhi.shkruajshqip.entity.User;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+public class AuditorAwareImpl implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.ofNullable(SecurityContextHolder.getContext())
+                .map(SecurityContext::getAuthentication)
+                .filter(Authentication::isAuthenticated)
+                .map(Authentication::getPrincipal)
+                .filter(principal -> principal instanceof User)
+                .map(principal -> (((User) principal).getUsernameOfUser()));
+    }
+}
--- src/main/java/dev/ardijorganxhi/shkruajshqip/entity/base/BaseEntity.java
+++ src/main/java/dev/ardijorganxhi/shkruajshqip/entity/base/BaseEntity.java
@@ -5,7 +5,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -24,10 +26,17 @@ public abstract class BaseEntity {
     @Column(updatable = false)
     private Date createdDate;
 
+    @CreatedBy
+    @Column(updatable = false)
+    private String createdBy;
+
     @LastModifiedDate
     @Temporal(TemporalType.TIMESTAMP)
-    @Column(updatable = false)
     private Date lastModifiedDate;
+
+    @Column
+    @LastModifiedBy
+    private String lastModifiedBy;
 
     @Column(nullable = false)
     private boolean isActive = true;
